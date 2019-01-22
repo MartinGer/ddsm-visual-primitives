64aad87d036badb14f753d37b1e09a3d1b700750
Martin Gerstmaier <gerstmaier.martin@gmx.net>
2019-01-22 13:35:57 +0100

    fix file upload route

--- server/server.py
+++ server/server.py
@@ -142,46 +142,60 @@ def handle_survey_ranked():
 def upload_file():
     file = request.files['image']
     full_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-
-    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
     file.save(full_path)
 
-    return render_template('single_image.html', success=True, full_path=full_path)
-
-
-@app.route('/process_image', methods=['POST'])
-def process_image():
-    original_path = os.path.join(app.config['UPLOAD_FOLDER'],  'test.jpg')
-    processed_path = os.path.join(app.config['PROCESSED_FOLDER'], 'benign.jpg')
-    result = backend.single_image_analysis.analyze_one_image(os.path.join('../server/static/uploads', 'benign.jpg'))
-
-    activation_map_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'activation.jpg')
-
-    top_units_and_activations = result.get_top_units(result.classification, 1)
-    activation_map = top_units_and_activations[0][2]  # activation map for unit 0 => top unit
-
-    img = Image.open(original_path)
-    # normalize activation values between 0 and 255
-    activation_map_normalized = backend.normalize_activation_map(activation_map)
-
-    # resize activation map to img size
-    activation_map_resized = backend.resize_activation_map(img, activation_map_normalized)
-
-    plt.gray()  # grayscale
-    plt.imsave(activation_map_path, activation_map_resized)
-
-    activations_overlayed_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'benign.jpg')
-    img.save(activations_overlayed_path)
-
-    backend.grad_cam()
-    return render_template('single_image.html', success=False, processed=True, full_path=processed_path,
-                           top_units_and_activations=top_units_and_activations,
-                           activation_map_path=activation_map_path, activations_overlayed_path=activations_overlayed_path)
+    return render_template('single_image.html', success=True, full_path=full_path, image_filename=file.filename)
 
 
 @app.route('/unit_ranking_by_weights/<training_session>/<checkpoint_name>/upload')
 def single_image(training_session, checkpoint_name):
-    return render_template('single_image.html', success=False, processed=False, full_path='')
+    return render_template('single_image.html', success=False, processed=False)
+
+
+@app.route('/image_/<image_filename>')
+def image_(image_filename):
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
+    preprocessed_full_image = get_preview_of_preprocessed_image(image_path)
+    preprocessed_full_image_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'full_image_{}.jpg'.format(uuid.uuid4()))
+    preprocessed_full_image.save(preprocessed_full_image_path)
+    result = backend.single_image_analysis.analyze_one_image(image_path)
+
+    mask_dirs = ["benigns", "benign_without_callbacks", "cancers"]
+    mask_path = ""
+    for mask_dir in mask_dirs:
+        orig_mask_path = os.path.join('../data/ddsm_masks/3class', mask_dir, image_filename[:-4] + '.png')
+        if os.path.exists(orig_mask_path):
+            mask_preprocessed = get_preview_of_preprocessed_image(orig_mask_path)
+            mask_preprocessed = ImageOps.colorize(ImageOps.equalize(mask_preprocessed), (0, 0, 0), (255, 0, 0))
+            mask_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'mask_{}.png'.format(uuid.uuid4()))
+            mask_preprocessed.save(mask_path)
+
+    units_to_show = 10
+    top_units_and_activations = result.get_top_units(result.classification, units_to_show)
+
+    activation_maps = []
+
+    for i in range(units_to_show):
+        activation_map = top_units_and_activations[i][2]  # activation map for unit with rank i
+        act_map_img = backend.to_heatmap(activation_map)
+        act_map_img = act_map_img.resize(preprocessed_full_image.size, resample=Image.BICUBIC)
+
+        activation_map_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'activation_{}.jpg'.format(uuid.uuid4()))
+        act_map_img.save(activation_map_path, "JPEG")
+        activation_maps.append(activation_map_path)
+
+    preprocessing_descr = preprocessing_description()
+
+    return render_template('image.html',
+                           image_path=result.image_path,
+                           preprocessed_full_image_path=preprocessed_full_image_path,
+                           mask_path=mask_path,
+                           checkpoint_path=result.checkpoint_path,
+                           preprocessing_descr=preprocessing_descr,
+                           classification=result.classification,
+                           class_probs=result.class_probs,
+                           top_units_and_activations=top_units_and_activations,
+                           activation_maps=activation_maps)
 
 
 @app.route('/example_analysis')
