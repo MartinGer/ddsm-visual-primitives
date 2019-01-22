6b3a15ae8317d751692d57f5a00ad92d4d6da381
Martin Gerstmaier <gerstmaier.martin@gmx.net>
2019-01-22 14:15:03 +0100

    remove doubled routes

--- server/server.py
+++ server/server.py
@@ -143,7 +143,6 @@ def upload_file():
     file = request.files['image']
     full_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(full_path)
-
     return render_template('single_image.html', success=True, full_path=full_path, image_filename=file.filename)
 
 
@@ -152,60 +151,9 @@ def single_image(training_session, checkpoint_name):
     return render_template('single_image.html', success=False, processed=False)
 
 
-@app.route('/image_/<image_filename>')
-def image_(image_filename):
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
-    preprocessed_full_image = get_preview_of_preprocessed_image(image_path)
-    preprocessed_full_image_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'full_image_{}.jpg'.format(uuid.uuid4()))
-    preprocessed_full_image.save(preprocessed_full_image_path)
-    result = backend.single_image_analysis.analyze_one_image(image_path)
-
-    mask_dirs = ["benigns", "benign_without_callbacks", "cancers"]
-    mask_path = ""
-    for mask_dir in mask_dirs:
-        orig_mask_path = os.path.join('../data/ddsm_masks/3class', mask_dir, image_filename[:-4] + '.png')
-        if os.path.exists(orig_mask_path):
-            mask_preprocessed = get_preview_of_preprocessed_image(orig_mask_path)
-            mask_preprocessed = ImageOps.colorize(ImageOps.equalize(mask_preprocessed), (0, 0, 0), (255, 0, 0))
-            mask_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'mask_{}.png'.format(uuid.uuid4()))
-            mask_preprocessed.save(mask_path)
-
-    units_to_show = 10
-    top_units_and_activations = result.get_top_units(result.classification, units_to_show)
-
-    activation_maps = []
-
-    for i in range(units_to_show):
-        activation_map = top_units_and_activations[i][2]  # activation map for unit with rank i
-        act_map_img = backend.to_heatmap(activation_map)
-        act_map_img = act_map_img.resize(preprocessed_full_image.size, resample=Image.BICUBIC)
-
-        activation_map_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'activation_{}.jpg'.format(uuid.uuid4()))
-        act_map_img.save(activation_map_path, "JPEG")
-        activation_maps.append(activation_map_path)
-
-    preprocessing_descr = preprocessing_description()
-
-    return render_template('image.html',
-                           image_path=result.image_path,
-                           preprocessed_full_image_path=preprocessed_full_image_path,
-                           mask_path=mask_path,
-                           checkpoint_path=result.checkpoint_path,
-                           preprocessing_descr=preprocessing_descr,
-                           classification=result.classification,
-                           class_probs=result.class_probs,
-                           top_units_and_activations=top_units_and_activations,
-                           activation_maps=activation_maps)
-
-
-@app.route('/example_analysis')
-def example_analysis():
-    return image('cancer_05-C_0128_1.LEFT_CC.LJPEG.1.jpg')
-
-
 @app.route('/image/<image_filename>')
 def image(image_filename):
-    image_path = os.path.join('../data/ddsm_raw/', image_filename)
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
     preprocessed_full_image = get_preview_of_preprocessed_image(image_path)
     preprocessed_full_image_path = os.path.join(app.config['ACTIVATIONS_FOLDER'], 'full_image_{}.jpg'.format(uuid.uuid4()))
     preprocessed_full_image.save(preprocessed_full_image_path)
