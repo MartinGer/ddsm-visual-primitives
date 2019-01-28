ae6ee07b05c5e9768c1531351d87637461f7239b
Frederic Schneider <frederic.schneider@student.hpi.uni-potsdam.de>
2019-01-28 18:30:04 +0100

    Full image on patch hover

--- server/static/survey.js
+++ server/static/survey.js
@@ -23,6 +23,18 @@ window.onload = function() {
             subList.style.display = 'none';
         }
     });
+
+    var tooltips = document.querySelectorAll('.tooltip-parent span');
+
+    window.onmousemove = function (e) {
+        var x = (e.clientX + 20) + 'px',
+            y = (e.clientY + 20) + 'px';
+        for (var i = 0; i < tooltips.length; i++) {
+            tooltips[i].style.top = y;
+            tooltips[i].style.left = x;
+        }
+    };
+
 };
 
 function updateShowPhenomena() {
