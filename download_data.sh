edb24f4f842652900971c1e75fdb932bd90edef6
Frederic Schneider <frederic.schneider@student.hpi.uni-potsdam.de>
2019-03-11 15:08:45 +0100

    Use curl as an alternative to wget in data dl script

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,15 @@
 cd data
-wget http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
-wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+if [type wget > /dev/null]; then
+  wget http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
+  wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
+  wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
+  wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+else
+  curl -O http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
+  curl -O http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
+  curl -O http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
+  curl -O http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+fi
 tar -xf ddsm_patches.tar.gz
 tar -xf ddsm_labels.tar.gz
 tar -xf ddsm_raw.tar.gz
