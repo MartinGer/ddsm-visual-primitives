a7ee5e4063bb6a228b20ccb33837e982f9ab7f98
Frederic Schneider <frederic.schneider@student.hpi.uni-potsdam.de>
2019-03-11 16:11:05 +0100

    Add ddsm_masks to download script

--- download_data.sh
+++ download_data.sh
@@ -4,13 +4,16 @@ if [type wget > /dev/null]; then
   wget http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
   wget http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
   wget http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+  wget http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
 else
   curl -O http://data.csail.mit.edu/places/medical/data/ddsm_patches.tar.gz
   curl -O http://data.csail.mit.edu/places/medical/data/ddsm_labels.tar.gz
   curl -O http://data.csail.mit.edu/places/medical/data/ddsm_raw.tar.gz
   curl -O http://data.csail.mit.edu/places/medical/data/ddsm_raw_image_lists.tar.gz
+  curl -O http://data.csail.mit.edu/places/medical/data/ddsm_masks.tar.gz
 fi
 tar -xf ddsm_patches.tar.gz
 tar -xf ddsm_labels.tar.gz
 tar -xf ddsm_raw.tar.gz
 tar -xf ddsm_raw_image_lists.tar.gz
+tar -xf ddsm_masks.tar.gz
